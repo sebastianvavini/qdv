c0f6dd02037d37e94f0cdfc238131142d118bca7
Sebastiao Vinicius Araujo de Moura <sebastianbavini@hotmail.com>
2024-04-18 18:50:48 -0300

    erro no binding

--- app/src/main/java/com/sebastiaomoura/qdv/MainActivity.kt
+++ app/src/main/java/com/sebastiaomoura/qdv/MainActivity.kt
@@ -4,11 +4,45 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(Medita())
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.new_game ->{
+                    loadFragment(Medita())
+                    true
+                }
+                R.id.new_sono ->{
+                    loadFragment(SonoFragment())
+                    true
+                }
+                R.id.fisico -> {
+                    loadFragment(FisicoFragment())
+                    true
+                }
+                R.id.alimento -> {
+                    loadFragment(AlimentoFragment())
+                    true
+                }
+
+                else -> {
+                    println("else aqui")
+                    true
+                }
+            }
+
+        }
+
     }
 
     // look at this section
@@ -16,5 +50,14 @@ class MainActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.basic_menu,menu)
         return super.onCreateOptionsMenu(menu)
+
+
+
+
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
